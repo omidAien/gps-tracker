4af1439441440704642478113636cf0972bdd0b7
Omid Aiene <omid.aienad@gmail.com>
2022-02-07 16:27:39 +0330

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'GPSTracker';
+
+  _horizontalPosition: MatSnackBarHorizontalPosition = "center";
+  _verticalPosition: MatSnackBarVerticalPosition = "top";
+
+  constructor(private snackBar: MatSnackBar) {
+    
+  }
+
+  ngOnInit(): void {
+      
+    this.getLocation();
+
+  }
+
+  getLocation() {
+
+    if (navigator.geolocation) {
+      
+      try {
+
+        navigator.geolocation.getCurrentPosition((position) => {
+
+          const res = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+    
+          this.snackBar.open(res, '', {
+            duration: 5000,
+            horizontalPosition: this._horizontalPosition,
+            verticalPosition: this._verticalPosition,
+          });
+
+        }, 
+        (error) => {
+
+          this.snackBar.open("Please Use GPS-DEVICE!", '', {
+            duration: 5000,
+            horizontalPosition: this._horizontalPosition,
+            verticalPosition: this._verticalPosition,
+          });
+
+        },
+        {
+          enableHighAccuracy: true,
+          maximumAge: 30000,
+          timeout: 27000
+        });
+
+      } catch (error) {
+        
+        this.snackBar.open("error", '', {
+          duration: 5000,
+          horizontalPosition: this._horizontalPosition,
+          verticalPosition: this._verticalPosition,
+        });
+
+      }
+      
+    } 
+    else {
+      const errorMessage:string = "Geolocation is not supported by this browser.";
+      
+      this.snackBar.open(errorMessage, '', {
+        duration: 5000,
+        horizontalPosition: this._horizontalPosition,
+        verticalPosition: this._verticalPosition,
+      });
+
+    }
+  }
+
 }
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,106 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from "@angular/material/icon";
+import { MatChipsModule } from '@angular/material/chips';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { PlatformModule } from '@angular/cdk/platform';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatListModule } from '@angular/material/list';
+import { MatSortModule } from '@angular/material/sort';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { MatRippleModule } from '@angular/material/core';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatChipsModule,
+    MatExpansionModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatProgressBarModule,
+    MatSnackBarModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatDialogModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    PlatformModule,
+    MatSelectModule,
+    MatDividerModule,
+    MatDatepickerModule,
+    MatTabsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatBottomSheetModule,
+    MatListModule,
+    MatSortModule,
+    MatBadgeModule,
+    MatRadioModule,
+    MatTooltipModule,
+    MatProgressSpinnerModule,
+    ScrollingModule,
+    MatRippleModule
+  ],
+  exports: [
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatChipsModule,
+    MatExpansionModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatProgressBarModule,
+    MatSnackBarModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatDialogModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    PlatformModule,
+    MatSelectModule,
+    MatDividerModule,
+    MatDatepickerModule,
+    MatTabsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatBottomSheetModule,
+    MatListModule,
+    MatSortModule,
+    MatBadgeModule,
+    MatRadioModule,
+    MatTooltipModule,
+    MatProgressSpinnerModule,
+    ScrollingModule,
+    MatRippleModule
+  ]
+})
+export class MaterialModule { }
